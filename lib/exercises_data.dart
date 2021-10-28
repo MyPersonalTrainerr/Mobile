f851ce84d42bfe0ac086cbd511d42085a8f9906d
khloud-azeem <khloud.mostafa00@eng-st.cu.edu.eg>
2021-10-28 23:46:06 +0200

    add dummy exercises data

---
+++ lib/exercises_data.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import '../models/Exercise.dart';
+
+const List<Exercise> ExercisesData = [
+  Exercise(
+    id: 'w1',
+    title: 'Squat',
+    imageUrl: 'images/squat.jpg',
+  ),
+  Exercise(
+    id: 'w2',
+    title: 'Push Up',
+    imageUrl: 'images/pushup.jpg',
+  ),
+  Exercise(
+    id: 'w3',
+    title: 'Split',
+    imageUrl: 'images/split.jpg',
+  ),
+];
