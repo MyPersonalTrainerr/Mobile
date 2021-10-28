e43725509ead8914009e18d7ecb784c8c5f079b3
khloud-azeem <khloud.mostafa00@eng-st.cu.edu.eg>
2021-10-28 23:37:31 +0200

    add class to define an exercise

---
+++ lib/models/exercise.dart
@@ -0,0 +1,11 @@
+class Exercise {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  const Exercise({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+  });
+}
