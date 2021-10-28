472adfe0dddf007c586753eed8700d4f655b5690
khloud-azeem <khloud.mostafa00@eng-st.cu.edu.eg>
2021-10-28 22:14:40 +0200

    add exersices overview screen

---
+++ lib/screens/exersices_overview.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ExersicesOverviewScreen extends StatelessWidget {
+  const ExersicesOverviewScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Your Personal Trainer'),),
+    );
+  }
+}
