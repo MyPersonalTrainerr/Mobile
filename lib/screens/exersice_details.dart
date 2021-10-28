9b00e4ff0ea008ba9e79ca53dab7fc0e737d5036
khloud-azeem <khloud.mostafa00@eng-st.cu.edu.eg>
2021-10-28 23:10:57 +0200

    add exercise details screen & exercise item widget

---
+++ lib/screens/exersice_details.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ExersiceDetailsScreen extends StatelessWidget {
+  const ExersiceDetailsScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/widgets/exersice_item.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ExerciseItem extends StatelessWidget {
+
+  final String id;
+  final String title;
+  final String imageUrl;
+  ExerciseItem({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
