c474dfff4eefeaefd01db764e9eaeeecb623f356
khloud-azeem <khloud.mostafa00@eng-st.cu.edu.eg>
2021-11-01 23:41:14 +0200

    add an exercise details screen content

--- lib/screens/exersice_details.dart
+++ lib/screens/exersice_details.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:my_personal_trainer/providers/exercises_provider.dart';
+import 'package:my_personal_trainer/widgets/exercise_item_content.dart';
+import 'package:my_personal_trainer/widgets/exersice_item.dart';
+import 'package:provider/provider.dart';
 
-class ExersiceDetailsScreen extends StatelessWidget {
-  const ExersiceDetailsScreen({ Key? key }) : super(key: key);
+class ExersiceDetailsScreen extends StatefulWidget {
+  const ExersiceDetailsScreen({Key? key}) : super(key: key);
+  static const routeName = '/exercise-details';
 
   @override
+  State<ExersiceDetailsScreen> createState() => _ExersiceDetailsScreenState();
+}
+
+class _ExersiceDetailsScreenState extends State<ExersiceDetailsScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final routeArgs = ModalRoute.of(context)!.settings.arguments;
+    final exerciseId = routeArgs;
+
+    final exercisesData =
+        Provider.of<ExercisesProvider>(context, listen: false);
+    final selectedExercise = exercisesData.findItemById(exerciseId as String);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Exercise Details',
+        ),
+      ),
+      body: ExerciseItemContent(selectedExercise),
     );
   }
 }
