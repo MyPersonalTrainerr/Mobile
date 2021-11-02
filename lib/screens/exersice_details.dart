1eb9d65d83964c813f52b6b9649a5ad030bcd4d2
khloud-azeem <khloud.mostafa00@eng-st.cu.edu.eg>
2021-11-02 02:02:00 +0200

    add camera, capture a video

--- lib/screens/exersice_details.dart
+++ lib/screens/exersice_details.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:my_personal_trainer/providers/exercises_provider.dart';
 import 'package:my_personal_trainer/widgets/exercise_item_content.dart';
-import 'package:my_personal_trainer/widgets/exersice_item.dart';
 import 'package:provider/provider.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ExersiceDetailsScreen extends StatefulWidget {
   const ExersiceDetailsScreen({Key? key}) : super(key: key);
@@ -13,6 +16,18 @@ class ExersiceDetailsScreen extends StatefulWidget {
 }
 
 class _ExersiceDetailsScreenState extends State<ExersiceDetailsScreen> {
+  Future<void> takePicture() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? video = await _picker.pickVideo(
+      source: ImageSource.camera,
+      maxDuration: Duration(seconds: 3),
+    );
+    final File videoFile = File(video!.path);
+    final appDirectory = await syspaths.getApplicationDocumentsDirectory();
+    final videoName = path.basename(video.path);
+    final savedVideo = await videoFile.copy('${appDirectory.path}/$videoName');
+  }
+
   @override
   Widget build(BuildContext context) {
     final routeArgs = ModalRoute.of(context)!.settings.arguments;
@@ -28,7 +43,27 @@ class _ExersiceDetailsScreenState extends State<ExersiceDetailsScreen> {
           'Exercise Details',
         ),
       ),
-      body: ExerciseItemContent(selectedExercise),
+      body: Column(
+        children: <Widget>[
+          ExerciseItemContent(selectedExercise),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Try Yourself'),
+                Card(
+                  elevation: 5,
+                  child: IconButton(
+                    onPressed: takePicture,
+                    icon: Icon(Icons.video_camera_back_outlined),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
