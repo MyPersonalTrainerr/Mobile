f452c28b5c5bdc9f7eec345b6b4e0464d9d30dec
khloud-azeem <khloud.mostafa00@eng-st.cu.edu.eg>
2021-11-04 01:30:35 +0200

    add app drawer

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:my_personal_trainer/screens/user_sign.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            title: const Text('Keep It Up!'),
+            automaticallyImplyLeading: false,
+          ),
+          ListTile(
+            leading: const Icon(Icons.line_weight),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+            title: const Text('Exercises'),
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(UserSignScreen.routeName);
+            },
+            title: const Text('Account'),
+          ),
+          const Divider(),
+        ],
+      ),
+    );
+  }
+}
