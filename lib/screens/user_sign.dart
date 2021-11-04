1d0b24564e0283216602c519285d93740aa7476d
khloud-azeem <khloud.mostafa00@eng-st.cu.edu.eg>
2021-11-04 18:54:21 +0200

    save form data, post & fetch from server functions

--- lib/screens/user_sign.dart
+++ lib/screens/user_sign.dart
@@ -1,15 +1,57 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:my_personal_trainer/widgets/app_drawer.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class UserSignScreen extends StatelessWidget {
+class UserSignScreen extends StatefulWidget {
   const UserSignScreen({Key? key}) : super(key: key);
   static const routeName = '/user-sign';
+
+  @override
+  State<UserSignScreen> createState() => _UserSignScreenState();
+}
+
+class _UserSignScreenState extends State<UserSignScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> userData = [];
+
+  void _postRequest(List data) async {
+    const url = "http://192.168.1.4:8000/Test";
+    final response = await http.post(
+      Uri.parse(url),
+      body: json.encode(data),
+    );
+  }
+
+  Future<List<dynamic>> _fetchTaks() async {
+    const url = "http://192.168.1.4:8000/Test";
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body) as List;
+      return data;
+    } else {
+      return [];
+    }
+  }
+
+  void _saveForm() {
+    _formKey.currentState!.save();
+    _postRequest(userData);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Account'),
+        title: const Text('Account'),
+        actions: [
+          IconButton(
+            onPressed: () => _saveForm(),
+            icon: const Icon(Icons.save),
+          ),
+        ],
       ),
       body: Form(
         child: Container(
@@ -20,27 +62,33 @@ class UserSignScreen extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 Container(
-                  child: Text('Sign In'),
+                  child: const Text('Sign In'),
                 ),
                 TextFormField(
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     label: Text('Enter your email...'),
                   ),
                   onFieldSubmitted: (_) {},
-                  onSaved: (value) {},
-                  validator: (value) {},
+                  textInputAction: TextInputAction.next,
+                  onSaved: (value) {
+                    userData.add(value as String);
+                  },
                 ),
                 TextFormField(
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     label: Text('Enter your password...'),
                   ),
-                  onFieldSubmitted: (_) {},
-                  onSaved: (value) {},
-                  validator: (value) {},
+                  onFieldSubmitted: (_) {
+                    _saveForm();
+                  },
+                  textInputAction: TextInputAction.done,
+                  onSaved: (value) {
+                    userData.add(value as String);
+                  },
                 ),
                 TextButton(
-                  onPressed: () {},
-                  child: Text('LOGIN'),
+                  onPressed: _saveForm,
+                  child: const Text('LOGIN'),
                 ),
               ],
             ),
