9f3facd50b7f4f764b24ca5e733a66042e0e49f4
khloud-azeem <khloud.mostafa00@eng-st.cu.edu.eg>
2022-04-23 16:57:23 +0200

    add logging test

--- test/logging_test.dart
+++ test/logging_test.dart
@@ -1,16 +1,17 @@
-// import 'package:my_personal_trainer/widgets/auth_card.dart';
-// import 'package:test/test.dart';
+import 'package:my_personal_trainer/widgets/auth_card.dart';
+import 'package:my_personal_trainer/widgets/auth_form.dart';
+import 'package:test/test.dart';
 
-// void main() {
-//   group('email validation', () {
-//     test('empty email address returns error', () {
-//       var result = EmailFieldValidator.validate('');
-//       expect(result, 'Please enter a valid email adress');
-//     });
+void main() {
+  group('email validation', () {
+    test('empty email address returns error', () {
+      var result = EmailFieldValidator.validate('');
+      expect(result, 'Please enter a valid email adress');
+    });
 
-//     test('non empty email adress returns null', () {
-//       var result = EmailFieldValidator.validate('kk@g.c');
-//       expect(result, null);
-//     });
-//   });
-// }
+    test('non empty email adress returns null', () {
+      var result = EmailFieldValidator.validate('kk@g.c');
+      expect(result, null);
+    });
+  });
+}
